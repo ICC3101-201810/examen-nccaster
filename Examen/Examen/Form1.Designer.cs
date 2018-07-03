89832fc5a4e801650a0448ac1f2636719c20dedf
¨Nicolas¨ <jatellez@miuandes.cl>
2018-07-03 11:27:42 -0400

    examen

---
+++ Examen/Examen/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Examen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.aNumber = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.atextbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.BackColor = System.Drawing.SystemColors.Window;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(117, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(51, 45);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.move_it_move);
+            // 
+            // button2
+            // 
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(51, 43);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.Location = new System.Drawing.Point(225, 201);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(47, 48);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // aNumber
+            // 
+            this.aNumber.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.aNumber.AutoSize = true;
+            this.aNumber.BackColor = System.Drawing.SystemColors.Window;
+            this.aNumber.Location = new System.Drawing.Point(114, 12);
+            this.aNumber.Name = "aNumber";
+            this.aNumber.Size = new System.Drawing.Size(0, 13);
+            this.aNumber.TabIndex = 3;
+            this.aNumber.Click += new System.EventHandler(this.aNumber_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button4.Image = global::Examen.Properties.Resources.cherry;
+            this.button4.Location = new System.Drawing.Point(200, 29);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(45, 38);
+            this.button4.TabIndex = 4;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 30000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 10000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // atextbox
+            // 
+            this.atextbox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.atextbox.Location = new System.Drawing.Point(-1, 9);
+            this.atextbox.Multiline = true;
+            this.atextbox.Name = "atextbox";
+            this.atextbox.Size = new System.Drawing.Size(340, 43);
+            this.atextbox.TabIndex = 5;
+            this.atextbox.Text = "Bienvenido a Pacman\r\nPara jugar usar W,A,S,D\r\n";
+            this.atextbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.atextbox.TextChanged += new System.EventHandler(this.atextbox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.atextbox);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.aNumber);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pacman";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label aNumber;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.TextBox atextbox;
+    }
+}
+
---
+++ Examen/Examen/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Examen
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            KeyDown += new KeyEventHandler(move_it_move);
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void move_it_move(object sender, KeyEventArgs e)
+        {
+            
+            switch (e.KeyData)
+            {
+                case Keys.D:
+                    button1.Location = new Point(button1.Left + 4, button1.Top);
+                    break;
+                case Keys.A:
+                    button1.Location = new Point(button1.Left - 4, button1.Top);
+                    break;
+                case Keys.W:
+                    button1.Location = new Point(button1.Left, button1.Top - 4);
+                    break;
+                case Keys.S:
+                    button1.Location = new Point(button1.Left, button1.Top + 4);
+                    break;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Timer tm = new Timer();
+
+            if ((button2.Location == button1.Location) || (button3.Location == button1.Location))
+            {
+                Application.Exit();
+            }
+            button2.Location = new Point(button2.Location.X + 4 , button2.Location.Y + 4);
+            button3.Location = new Point(button3.Location.X - 4, button3.Location.Y - 4);
+            
+        }
+
+        private void aNumber_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            button4.Visible = true;
+            Random rn = new Random();
+            int x = rn.Next(1, 501);
+            int y = rn.Next(1, 501);
+            button4.Location = new Point(x, y);
+            button4.Visible = true;
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            atextbox.Visible = false;
+            //if (button4.Visible == true) { button4.Visible = false; }
+        }
+
+        private void atextbox_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
